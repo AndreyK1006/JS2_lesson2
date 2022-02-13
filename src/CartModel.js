3b987e5f0c94b15febbb9f6f7ee309f85b85889e
AndreyK <kavaboonga007@gmail.com>
2022-02-13 21:33:21 +0500

    2

---
+++ src/CartModel.js
@@ -0,0 +1,41 @@
+import ProductList from './ProductList.js'
+
+export default class CartModel extends ProductList {
+  constructor(apiHandler, eventEmmiter) {
+    super([])
+    this.api = apiHandler
+    this.eventEmmiter = eventEmmiter
+  }
+
+  fetch() {
+    return this.api.getCart(
+      (list) => {
+        this.list = list
+        this.eventEmmiter.emit('cartFeched', list)
+      }
+    )
+  }
+
+  add(product, onError) {
+    this.api.addToCart(
+      () => {
+        this.list.push(product)
+      },
+      onError,
+      product
+    )
+  }
+
+  remove(id, onError) {
+    if(this.find(id)) {
+      this.api.removeFromCart(
+        () => {
+          this.remove(id)
+        },
+        onError,
+        this.list[index]
+      )
+    }
+  }
+
+}
